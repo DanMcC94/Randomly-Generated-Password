78bb1f58db8a4e511d00fa7b7f7d796527f433a5
DM94 <danmccartney94@gmail.com>
2020-09-24 22:07:08 -0500

    Tried to make an array to add for character set.

--- script.js
+++ script.js
@@ -17,8 +17,17 @@ function writePassword() {
 
 }
 
+//var lowerCase [q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,];
+//var upperCase [Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,];
+//var numbers [0,1,2,3,4,5,6,7,8,9];
+//var special [!,@,#,$,%,^,&,*,(,),_,+,=,~,`]; 
 
+ 
+//Random generator
 function generatePassword() {
+
+
+
     var length = choosingLength;
         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+<>?:\"\}\{\~\`.,",
         retVal = "";
