f00165dbb1665f6da8db4734b4d5066918111813
DM94 <danmccartney94@gmail.com>
2020-10-01 10:46:24 -0500

    Fixed the if statements to fix generator.

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ var choosingLower = confirm("Would you like lower case letters?");
 var choosingUpper = confirm("Would you like uppercase letters?");
 var choosingNumbers = confirm("Would you like numbers?");
 var choosingSpecialCharacters = confirm("Would you like special characters included?")
- 
+var finalPassword = "";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    finalPassword  = "";
 
-  passwordText.value = password;
+    var passwordText = document.querySelector("#password");
+  
+
+
+    var password = generatePassword();
+  
+
+    passwordText.value = password;
 
 }
 
-//var lowerCase [q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,];
-//var upperCase [Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,];
-//var numbers [0,1,2,3,4,5,6,7,8,9];
-//var special [!,@,#,$,%,^,&,*,(,),_,+,=,~,`]; 
+var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm',];
+var upperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@",'#', '$','%','^','&','*', '(',')','_', '+','=','~'];
+var options = [];
+
 
 //ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+<>?:\"\}\{\~\`.,
 //Random generator
@@ -28,36 +36,43 @@ function generatePassword() {
 
     if (choosingLower) {
 
-    var length = choosingLength;
-        charset = "abcdefghijklmnopqrstuvwxyz",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+        options = [...options, ...lowerCase]
+        //var length = choosingLength;
+        //charset = "abcdefghijklmnopqrstuvwxyz",
+        //retVal = "";
+        //for (var i = 0, n = charset.length; i < length; ++i) {
+        //retVal += charset.charAt(Math.floor(Math.random() * n));
+        //}
+        //return retVal;
     }
-    return retVal;
-}
-    
-    else if (choosingUpper) {
-    
-    var length = choosingLength;
-        charset = "QWERTYUIOPASDFGHJKLZXCVBNM",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-            retVal += charset.charAt(Math.floor(Math.random() * n));
-        }
-    return retVal;
+
+    if (choosingUpper) {
+
+        options = [...options, ...upperCase]
+
+        //var length = choosingLength;
+        //charset = "QWERTYUIOPASDFGHJKLZXCVBNM",
+        //retVal = "";
     }
 
     if (choosingSpecialCharacters) {
-    var length = choosingLength;
-        charset = "!@#$%^&*()_+~`,./<>?:|\\"
-    for (var i = 0, n = charset.length; i < length; ++i) {
-            retVal += charset.charAt(Math.floor(Math.random() * n));
-        }
-    return retVal;    
+        options = [...options, ...special]
+        // var length = choosingLength;
+        // charset = "!@#$%^&*()_+~`,./<>?:|\\"
+        //for (var i = 0, n = charset.length; i < length; ++i) {
+        //      retVal += charset.charAt(Math.floor(Math.random() * n));
+        // }
+        //return retVal;    
     }
 
 
+        for (var i = 0;  i < parseInt(choosingLength); i++) {
+
+                var randomIndex = Math.floor(Math.random() * options.length);
+              finalPassword = finalPassword + options[randomIndex];
+         }
+
+         return finalPassword;
 }
 
 // Add event listener to generate button
